a68acd3d0529e7cf6059c3327a5e30eba0f166b1
rgilfanov <rgilfanov@fix.ru>
2018-11-18 15:52:52 +0200

    Chapter 11

--- app/src/main/java/pro/ruzzy/criminalintent/CrimeListFragment.java
+++ app/src/main/java/pro/ruzzy/criminalintent/CrimeListFragment.java
@@ -74,15 +74,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
-            startActivityForResult(intent, REQUEST_CRIME);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CRIME) {
-
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
